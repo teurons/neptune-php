bfe50255cc132aa6b6874e952f775d3c77e987c2
Rajiv Seelam <rajiv@betalectic.com>
2022-05-20 16:37:16 +0530

    Require php curl

--- config/neptune.php
+++ config/neptune.php
@@ -3,5 +3,5 @@
 return [
     'edge' => "https://edge.staging.teurons.com/neptune",
     'token' => env('NEPTUNE_TOKEN', ""),
-    'env' => env('NEPTUNE_ENV', ""),
+    'env' => env('NEPTUNE_ENV', "dev"),
 ];
