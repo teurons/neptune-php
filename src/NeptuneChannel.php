3203b498177e6011dd283cfb6c0d9cdf3b578feb
Rajiv Seelam <rajiv@betalectic.com>
2022-05-23 18:31:03 +0530

    Update neptune channel for laravel

--- src/NeptuneChannel.php
+++ src/NeptuneChannel.php
@@ -18,55 +18,16 @@ class NeptuneChannel
     public function send($notifiable, Notification $notification)
     {
         $payload = $notification->toNeptune($notifiable);
-        
-        $notifiableArray = (array) $notifiable;
+        $eventType = $payload['event_type'];
+        $data = $payload['data'];
+        $remaining = $payload;
+        unset($remaining['data']);
+        unset($remaining['event_type']);
 
-        $recipients = [];
-
-        if (!empty($notifiableArray[0]) && is_array($notifiableArray[0])) {
-            foreach ($notifiableArray as $key => $recipient) {
-                array_push($recipients, $this->getRecipientInfo((object)$recipient));
-            }
-        } else {
-            array_push($recipients, $this->getRecipientInfo($notifiable));            
-        }
-
-
-        $neptune = new Neptune($payload, $recipients);
-
-        if(isset($notification->notificationUUID) && $notification->notificationUUID != "" && !is_null($notification->notificationUUID)){
-            $neptune->fire($notification->notificationUUID, 'uuid');
-        }
-
-        
-        if(isset($notification->notificationSlug) && $notification->notificationSlug != "" && !is_null($notification->notificationSlug)){
-            $neptune->fire($notification->notificationSlug);
-        }
+        $neptune = new Neptune();
+        $neptune->fire($eventType, $data, $remaining);
 
     }
 
-    public function getRecipientInfo($recipient) {
-        $data = [];
-        if (isset($recipient->name)){
-            $data['name'] = $recipient->name;
-        }
 
-        if (isset($recipient->email)){
-            $data['email'] = $recipient->email;
-        }
-
-        if (isset($recipient->mobile)){
-            $data['mobile'] = $recipient->mobile;
-        }
-
-        if (isset($recipient->cc)){
-            $data['cc'] = $recipient->cc;
-        }
-
-        if (isset($recipient->bcc)){
-            $data['bcc'] = $recipient->bcc;
-        }
-
-        return $data;
-    }
 }
